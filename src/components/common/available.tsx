a6994189ab5b69d23acb75fc446fcf3368dfe464
Janhavi Patil <patil.janhavi1207@gmail.com>
2024-06-24 22:30:11 +0530

    feat: fix responsiveness

--- src/components/common/available.tsx
+++ src/components/common/available.tsx
@@ -1,14 +1,9 @@
 import { Container } from "@/components/common/container";
 import React from "react";
-import { Spotlight } from "../ui/spotlight";
 
 const Available = () => {
   return (
     <Container>
-      <Spotlight
-        className="-top-40 left-0 md:left-60 md:-top-20"
-        fill="white"
-      />
       <div className="w-full flex flex-col md:flex-row justify-between lg:items-center gap-3  my-4 font-mono text-foreground/45">
         <p className=" text-sm font-extralight">Hey, I{"'"}m Janhavi</p>
 
